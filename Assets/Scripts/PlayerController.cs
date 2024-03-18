fa0d139de72372ae365cff126e29b1b46b1b7516
SHsus1122 <tjgurtn1122@naver.com>
2024-03-18 21:49:36 +0900

    PlayerController 적용 및 모듈식 구현

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 10f;
+    private float zBound = 7.5f;
+    private Rigidbody playerRb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+
+    // Moves the Player based on arrow key input
+    void MovePlayer()
+    {
+        // 유니티의 InputManager 에 설정되어 있는 키값들로 인해서 아래의 코드가 작동 가능해집니다.
+        // 즉, InputManager 의 Axes 의 Horizontal, Vertical 의 설정들을 읽어와서 방향에 맞게 speed 만큼 값을 곱해서 움직이게 됩니다.
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
+
+    // Prevent the player from leaving the top or bottom of the screen
+    void ConstrainPlayerPosition()
+    {
+        // 상, 하 움직임 제한 코드
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+    }
+}
